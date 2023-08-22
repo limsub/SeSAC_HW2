eb8344454a6c65521ed3c24af428f06c9e9a9e6e
limsub <limsss9076@naver.com>
2023-08-22 16:33:25 +0900

    first commit

---
+++ 0822hw/0822hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0822hw
+//
+//  Created by 임승섭 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
