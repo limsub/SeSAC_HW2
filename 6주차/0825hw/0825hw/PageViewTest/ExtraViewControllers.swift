991b53787d634c1d5aa89a37f65a497762dc6160
limsub <limsss9076@naver.com>
2023-08-25 15:38:44 +0900

    8/25 PageViewController 뷰 생성시점 확인

---
+++ 6주차/0825hw/0825hw/PageViewTest/ExtraViewControllers.swift
@@ -0,0 +1,304 @@
+//
+//  ExtraViewControllers.swift
+//  0825hw
+//
+//  Created by 임승섭 on 2023/08/25.
+//
+
+import UIKit
+import SnapKit
+
+/* ======================== 1 ======================== */
+class ViewController1: UIViewController {
+    
+    let label = {
+        let l = UILabel()
+        l.text = "1번"
+        l.textColor = .black
+        l.backgroundColor = .white
+        
+        return l
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        print("1 viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("1 viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("1 viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("1 viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("1 viewDidDisappear")
+    }
+}
+
+class ViewController2: UIViewController {
+    
+    let label = {
+        let l = UILabel()
+        l.text = "2번"
+        l.textColor = .black
+        l.backgroundColor = .white
+        
+        return l
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        print("2 viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("2 viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("2 viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("2 viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("2 viewDidDisappear")
+    }
+}
+
+
+class ViewController3: UIViewController {
+    
+    let label = {
+        let l = UILabel()
+        l.text = "3번"
+        l.textColor = .black
+        l.backgroundColor = .white
+        
+        return l
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        print("3 viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("3 viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("3 viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("3 viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("3 viewDidDisappear")
+    }
+}
+
+class ViewController4: UIViewController {
+    
+    let label = {
+        let l = UILabel()
+        l.text = "4번"
+        l.textColor = .black
+        l.backgroundColor = .white
+        
+        return l
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        print("4 viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("4 viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("4 viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("4 viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("4 viewDidDisappear")
+    }
+}
+
+
+class ViewController5: UIViewController {
+    
+    let label = {
+        let l = UILabel()
+        l.text = "5번"
+        l.textColor = .black
+        l.backgroundColor = .white
+        
+        return l
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        print("5 viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("5 viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("5 viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("5 viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("5 viewDidDisappear")
+    }
+}
+
+
+class ViewController6: UIViewController {
+    
+    let label = {
+        let l = UILabel()
+        l.text = "6번"
+        l.textColor = .black
+        l.backgroundColor = .white
+        
+        return l
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        print("6 viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("6 viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("6 viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("6 viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("6 viewDidDisappear")
+    }
+}
+
+
+
---
+++ 6주차/0825hw/0825hw/PageViewTest/OnboardingViewController.swift
@@ -0,0 +1,72 @@
+//
+//  OnboardingViewController.swift
+//  0825hw
+//
+//  Created by 임승섭 on 2023/08/25.
+//
+
+import UIKit
+
+class OnboardingViewController: UIPageViewController {
+    
+    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+        
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var list: [UIViewController] = [];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray2
+        
+        list = [ViewController1(), ViewController2(), ViewController3(), ViewController4(), ViewController5(), ViewController6()]
+        
+        connectProtocol()
+        
+        guard let first = list.first else { return }
+        setViewControllers([list[2]], direction: .forward, animated: true)
+        
+    }
+    
+
+}
+
+extension OnboardingViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    func connectProtocol() {
+        delegate = self
+        dataSource = self
+    }
+    
+    // 비포
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = list.firstIndex(of: viewController) else { return nil }
+        
+        return currentIndex <= 0 ? nil : list[currentIndex - 1]
+    }
+    
+    // 애프터
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = list.firstIndex(of: viewController) else { return nil }
+        
+        return currentIndex >= list.count - 1 ? nil : list[currentIndex + 1]
+    }
+    
+    
+    // 아래 똥글뱅이
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return list.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let first = viewControllers?.first, let index = list.firstIndex(of: first) else { return 0}
+        
+        return index
+    }
+}
